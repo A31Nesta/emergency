8164c98021531b126cc75506368d5badce78c9bd
A31Nesta <anthroponesta@gmail.com>
2025-05-24 18:41:44 +0200

    start to mess around with https

---
+++ src/https.rs
@@ -0,0 +1,58 @@
+use axum::{
+    handler::HandlerWithoutStateExt,
+    http::{uri::Authority, StatusCode, Uri},
+    response::Redirect,
+    BoxError,
+};
+use axum_extra::extract::Host;
+use std::net::SocketAddr;
+
+#[derive(Clone, Copy)]
+pub struct Ports {
+    pub http: u16,
+    pub https: u16,
+}
+
+pub async fn redirect_http_to_https(ports: Ports) {
+    fn make_https(host: &str, uri: Uri, https_port: u16) -> Result<Uri, BoxError> {
+        let mut parts = uri.into_parts();
+
+        parts.scheme = Some(axum::http::uri::Scheme::HTTPS);
+
+        if parts.path_and_query.is_none() {
+            parts.path_and_query = Some("/".parse().unwrap());
+        }
+
+        let authority: Authority = host.parse()?;
+        let bare_host = match authority.port() {
+            Some(port_struct) => authority
+                .as_str()
+                .strip_suffix(port_struct.as_str())
+                .unwrap()
+                .strip_suffix(':')
+                .unwrap(), // if authority.port() is Some(port) then we can be sure authority ends with :{port}
+            None => authority.as_str(),
+        };
+
+        parts.authority = Some(format!("{bare_host}:{https_port}").parse()?);
+
+        Ok(Uri::from_parts(parts)?)
+    }
+
+    let redirect = move |Host(host): Host, uri: Uri| async move {
+        match make_https(&host, uri, ports.https) {
+            Ok(uri) => Ok(Redirect::permanent(&uri.to_string())),
+            Err(error) => {
+                println!("failed to convert URI to HTTPS ({error})");
+                Err(StatusCode::BAD_REQUEST)
+            }
+        }
+    };
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], ports.http));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, redirect.into_make_service())
+        .await
+        .unwrap();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::{convert::Infallible, env, fs, net::SocketAddr};
+use std::{convert::Infallible, env, fs, net::SocketAddr, path::PathBuf};
 
 use axum::{response::{Html, IntoResponse}, routing::get, Router};
+use axum_server::tls_rustls::RustlsConfig;
 use handlers::generate;
+use https::{redirect_http_to_https, Ports};
 use state::StateStruct;
 use tower::service_fn;
 use tower_http::services::ServeDir;
@@ -9,6 +11,7 @@ use tower_http::services::ServeDir;
 mod state;
 mod handlers;
 mod dto;
+mod https;
 
 #[tokio::main]
 async fn main() {
@@ -16,8 +19,22 @@ async fn main() {
 
     // Get environment variables
     let frontend_path = env::var("EMG_FRONTEND_PATH").unwrap_or(String::from("/opt/emg_frontend"));
+    let certs_path = env::var("EMG_CERTS_PATH").unwrap_or(String::from("/opt/emg_certs"));
 
     let state = StateStruct::new_state();
+    let ports = Ports {
+        http: 7878,
+        https: 3000,
+    };
+    tokio::spawn(redirect_http_to_https(ports));
+
+    // HTTPS
+    let config = RustlsConfig::from_pem_file(
+        PathBuf::from(&certs_path).join("cert.pem"),
+        PathBuf::from(&certs_path).join("key.pem")
+    )
+        .await
+        .expect("No certificates found!");
 
     let service = ServeDir::new(&frontend_path).not_found_service(service_fn(move |_req| {
         let dir = frontend_path.clone();
@@ -37,9 +54,13 @@ async fn main() {
         // .layer(CorsLayer::permissive())
     ;
     
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    let addr = SocketAddr::from(([0, 0, 0, 0], ports.https));
+    // let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
     println!("Started!");
-    axum::serve(listener, router).await.unwrap();
+    // axum::serve(listener, router).await.unwrap();
+    axum_server::bind_rustls(addr, config)
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
 }
